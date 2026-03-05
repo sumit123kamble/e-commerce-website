86a91f9b3569fcec129550d120ff37e01600e670
Sumit Kamble <kamblesumit2001@gmail.com>
2026-03-05 18:19:35 +0530

    for redploying versel

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -99,7 +99,7 @@ const Home = () => {
         </div>
 
         <div className="hero-content">
-          <div className="hero-badge">🌿 Fresh &amp; Natural<br></br> under construction</div>
+          <div className="hero-badge">🌿 Fresh &amp; Natural<br></br> <b>under construction</b></div>
 
           <h1 className="hero-title">
             Welcome to
